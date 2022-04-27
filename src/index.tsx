94a2943980c6dbfbe5c3b91a0071c7a81f3b1f95
hash-luk <lucasgamerhd18@gmail.com>
2022-04-27 09:11:31 -0300

    Fixed bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './Global-Styles/globalStyles';
 
 const root = ReactDOM.createRoot(
@@ -13,5 +12,3 @@ root.render(
     <GlobalStyle />
   </React.StrictMode>
 );
-
-reportWebVitals();
